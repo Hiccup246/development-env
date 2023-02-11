cc0692af9e776f42c71464e392f3eefdbb692795
--global <james.watt529@outlook.com>
2023-02-11 14:40:57 +0000

    Complete static homebrew-setup script

--- homebrew-setup.sh
+++ homebrew-setup.sh
@@ -1,13 +1,23 @@
 #!/bin/zsh
-# Install homebrew package manager
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install homebrew and all packages
 
+# Install homebrew package manager
+if ! hash brew
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew update
+else
+    echo "You already have brew installed."
+fi
+
+
+# Install command line programs
 # Command line utilities
 brew install curl
 brew install wget
 
 # Code repostiry
-brew intsall git
+brew instsall git
 
 # Package managers
 brew install pnpm
@@ -28,4 +38,18 @@ brew install asdf
 # Optional installations
 brew install railway
 
+# Install GUI applications
+brew install --cask spotify
+brew install --cask discord
+brew install --cask visual-studio-code
+brew install --cask google-chrome
+brew install --cask discord
+brew install --cask postman
+brew install --cask firefox
+brew install --cask docker
 
+if [[ ! -d /Applications/iTerm.app ]]; then
+    brew install --cask iterm2
+else
+    echo "You already have iTerm2 installed."
+fi
