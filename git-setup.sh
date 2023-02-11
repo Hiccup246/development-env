94e97d4bd4e3278d2153857f8f07bcb4579eea99
--global <james.watt529@outlook.com>
2023-02-11 14:42:31 +0000

    Add git-setup script

---
+++ git-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+# Configure git for development
+
+# Make git always use ssh for github repos
+git config --global url."git@github.com:".insteadOf https://github.com/
+
+# Configure git email + name + display name
+if [ -n "$(git config --global user.email)" ]; then
+  echo "✔ Git email is set to $(git config --global user.email)"
+else
+  read -p 'What is your Git email address?: ' gitEmail
+  git config --global user.email "$gitEmail"
+fi
+
+if [ -n "$(git config --global user.name)" ]; then
+  echo "✔ Git display name is set to $(git config --global user.name)"
+else
+  read -p 'What is your Git display name (Firstname Lastname)?: ' gitName
+  git config --global user.name "$gitName"
+fi
