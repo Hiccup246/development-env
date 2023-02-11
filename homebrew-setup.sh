24f8eb17b5d491c5c260c0c2b7618d91d733f728
James Watt <james.watt529@outlook.com>
2023-02-11 12:35:04 +0000

    Add homebrew setup script

---
+++ homebrew-setup.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+# Install homebrew package manager
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Command line utilities
+brew install curl
+brew install wget
+
+# Code repostiry
+brew intsall git
+
+# Package managers
+brew install pnpm
+brew install npm
+brew install pip
+
+# Run time environments
+brew install deno
+brew install node
+
+# GUI Application installer
+brew install mas
+
+# Version managers
+brew install nvm
+brew install asdf
+
+# Optional installations
+brew install railway
+
+
