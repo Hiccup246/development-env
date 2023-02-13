024308c0124081fc23cecd0ac98b3835e188213b
toto <toto>
2023-02-13 20:11:23 +0000

    Complete ssh-setup script

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -2,90 +2,97 @@
 # Generate ssh keys for github global user name
 # Inspiration taken from https://gist.github.com/jexchan/2351996 and https://gist.github.com/oanhnn/80a89405ab9023894df7
 
-# Setup global ssh key
-# Capture user ssh key filename
-echo "Time to configure ssh keys"
-read -r -p 'Do you want to configure this ssh key using global git info? [y/N] ' useGlobalGitInfo
-
-case "$useGlobalGitInfo" in
-    [yY][eE][sS]|[yY]) 
-        setup_global_ssh_key
-        ;;
-esac
-
-read -r -p 'Do you want to configure this ssh key using local git info? [y/N] ' useLocalGitInfo
-
-case "$useLocalGitInfo" in
-    [yY][eE][sS]|[yY]) 
-        setup_local_ssh_key
-        ;;
-    *)
-        echo "No other ssh options available"
-        ;;
-esac
-
 setup_global_ssh_key() {
+    echo ""
+
     if [[ ! $(git config --global user.email) ]]; then
        echo "No global git email found, please set one"
        return 
     fi
 
-    read -r -p 'Enter desired filename of your ssh key: ' sshFileName
+    read 'ssh_file_name?Enter desired filename of your ssh key: '
 
     # Generate the ssh key using the captured filename and global git email
-    cd .ssh/
-    ssh-keygen -t rsa -C $(git config --global user.email) -f "$sshFileName"
+    cd ~/.ssh
+    ssh-keygen -t rsa -C $(git config --global user.email) -f "$ssh_file_name"
+    echo "Ssh key generated and saved in ~/.ssh/"
+
+    pbcopy < "${ssh_file_name}.pub"
     echo "Copied generated ssh key to clipboard"
-    pbcopy < "${sshFileName}.pub"
 
     # Setup ssh config file
     touch ~/.ssh/config
-    FILE=~/.ssh/config
 
-    echo "\nHost github.com\n" > FILE
-    echo "\tHostName github.com\n" > FILE
-    echo "\tIdentityFile ~/.ssh/${sshFileName}\n" > FILE
-    echo "\tIdentitiesOnly yes" > FILE
+    echo "\nHost github.com" >> config
+    echo "\tHostName github.com" >> config
+    echo "\tIdentityFile ~/.ssh/${ssh_file_name}" >> config
+    echo "\tIdentitiesOnly yes" >> config
 
     cd -
 
-    echo "ssh key generated and saved in ~/.ssh/"
-    ls ~/.ssh/
-
-    echo "Saving ssh key to ssh agent"
-    ssh-add -D
-    ssh-add "~/.ssh/${sshFileName}"
+    ssh-add ~/.ssh/${ssh_file_name}
+    echo "Ssh key saved to ssh agent"
+    echo "Global ssh key setup successfully!"
 }
 
 setup_local_ssh_key() {
-    if [[ ! $(git config user.email && git config user.name) ]]; then
-       echo "No local git email or user name found, please set one"
-       return 
+    echo ""
+
+    if [[ ! $(git config --local user.email) ]]; then
+        echo "No local git user email found, please set one"
+
+        if [[ ! $(git config --local user.name) ]]; then
+            echo "No local git user name found, please set one"
+        fi
+
+        return
     fi
 
-    read -r -p 'Enter desired filename of your ssh key: ' sshFileName
+    git_user_email=$(git config --local user.email)
+    git_user_name=$(git config --local user.name)
 
-    # Generate the ssh key using the captured filename and global git email
-    cd .ssh/
-    ssh-keygen -t rsa -C $(git config user.email) -f "$sshFileName"
+    read 'ssh_file_name?Enter desired filename of your ssh key: '
+
+    # Generate the ssh key using the captured filename and local git email
+    cd ~/.ssh
+    ssh-keygen -t rsa -C ${git_user_email} -f "$ssh_file_name"
+    echo "Ssh key generated and saved in ~/.ssh/"
+
+    pbcopy < "${ssh_file_name}.pub"
     echo "Copied generated ssh key to clipboard"
-    pbcopy < "${sshFileName}.pub"
 
     # Setup ssh config file
     touch ~/.ssh/config
-    FILE=~/.ssh/config
 
-    echo "\nHost github.com-${git config user.name}\n" > FILE
-    echo "\tHostName github.com\n" > FILE
-    echo "\tIdentityFile ~/.ssh/${sshFileName}\n" > FILE
-    echo "\tIdentitiesOnly yes" > FILE
+    echo "\nHost github.com-${git_user_name}" >> config
+    echo "\tHostName github.com" >> config
+    echo "\tIdentityFile ~/.ssh/${ssh_file_name}" >> config
+    echo "\tIdentitiesOnly yes" >> config
 
     cd -
 
-    echo "ssh key generated and saved in ~/.ssh/"
-    ls ~/.ssh/
-
-    echo "Saving ssh key to ssh agent"
-    ssh-add -D
-    ssh-add "~/.ssh/${sshFileName}"
+    ssh-add ~/.ssh/${ssh_file_name}
+    echo "Ssh key saved to ssh agent"
+    echo "Local ssh key setup successfully!"
 }
+
+# Setup global ssh key
+# Capture user ssh key filename
+echo "Time to configure ssh keys"
+read -q '?Do you want to configure a global ssh key using global git info? [y/n] '
+answer=$?
+
+if ((!$answer)); then
+    setup_global_ssh_key
+fi
+
+echo ""
+read -q '?Do you want to configure a local ssh key using local git info? [y/n] '
+answer=$?
+
+if ((!$answer)); then
+    setup_local_ssh_key
+fi
+
+echo ""
+echo "No other ssh options available"
