5173475afa91f0f2c0e4bdfecdac78aadbd76901
--global <james.watt529@outlook.com>
2023-02-13 01:06:37 +0000

    Add menu template to main script

--- main.sh
+++ main.sh
@@ -1,2 +1,22 @@
 #!/bin/zsh
 # Main program loop with user options
+
+echo "===Welcome to Hiccup246 development environment setup program==="
+echo "What do you want to do?"
+
+PS3="Select item please: "
+
+items=("Item 1" "Item 2" "Item 3")
+
+while true; do
+    select item in "${items[@]}" Quit
+    do
+        case $REPLY in
+            1) echo "Selected item #$REPLY which means $item"; break;;
+            2) echo "Selected item #$REPLY which means $item"; break;;
+            3) echo "Selected item #$REPLY which means $item"; break;;
+            $((${#items[@]}+1))) echo "We're done!"; break 2;;
+            *) echo "Ooops - unknown choice $REPLY"; break;
+        esac
+    done
+done
