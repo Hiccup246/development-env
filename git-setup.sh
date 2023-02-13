596c817268a7270e0a2fea418f4d4ab287ef10a4
--global <james.watt529@outlook.com>
2023-02-13 17:59:41 +0000

    Complete git-setup script

--- git-setup.sh
+++ git-setup.sh
@@ -4,17 +4,74 @@
 # Make git always use ssh for github repos
 git config --global url."git@github.com:".insteadOf https://github.com/
 
-# Configure git email + name + display name
+configure_global_git_email() {
+  read 'git_email?What do you want your global git email address to be?: '
+  git config --global user.email "$git_email"
+  echo "✔ Git email is now set to $(git config --global user.email)"
+}
+
+configure_global_git_display_name() {
+  read 'git_display_name?What do you want your global git display name to be (Firstname Lastname)?: '
+  git config --global user.displayname "$git_display_name"
+  echo "✔ Git display name is now set to $(git config --global user.displayname)"
+
+}
+
+configure_global_git_name() {
+  read 'git_name?What do you want your global git name to be (Firstname Lastname)?: '
+  git config --global user.name "$git_name"
+  echo "✔ Git name is now set to $(git config --global user.name)"
+}
+
+# Configure git email
 if [ -n "$(git config --global user.email)" ]; then
   echo "✔ Git email is set to $(git config --global user.email)"
+
+  read -q "?Do you want to change your global git email? [y/n] "
+  answer=$?
+
+  if ((!$answer)); then
+    echo ""
+    configure_global_git_email
+  fi
 else
-  read -p 'What is your Git email address?: ' gitEmail
-  git config --global user.email "$gitEmail"
+  configure_global_git_email
 fi
 
+echo ""
+
+# Configure git name
 if [ -n "$(git config --global user.name)" ]; then
-  echo "✔ Git display name is set to $(git config --global user.name)"
+  echo "✔ Git name is set to $(git config --global user.name)"
+
+  read -q "?Do you want to change your global git name? [y/n] "
+  answer=$?
+
+  if ((!$answer)); then
+    echo ""
+    configure_global_git_name
+  fi
 else
-  read -p 'What is your Git display name (Firstname Lastname)?: ' gitName
-  git config --global user.name "$gitName"
+  configure_global_git_name
 fi
+
+echo ""
+
+# Configure git display name
+if [ -n "$(git config --global user.displayname)" ]; then
+  echo "✔ Git display name is set to $(git config --global user.displayname)"
+  
+  read -q "?Do you want to change your global git display name? [y/n] "
+  answer=$?
+
+  if ((!$answer)); then
+    echo ""
+    configure_global_git_display_name
+  fi
+else
+  configure_global_git_display_name
+fi
+
+echo ""
+echo ""
+echo "Git global info configured..."
