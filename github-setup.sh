bb9b70becd7c0cf7d646033542b7c5346d22f823
--global <james.watt529@outlook.com>
2023-02-13 17:22:51 +0000

    Complete github-setup script

--- github-setup.sh
+++ github-setup.sh
@@ -2,23 +2,35 @@
 # Download all public, non-archived, non-forked github repos for a github user into the current directory
 # Inspiration taken from https://gist.github.com/santoshphegde/218e26317327f9c5aa9791ad767c1b3e
 
-# Configure github user to clone repos from
-read -r -p "Do you want to clone all public, non-archived, non-forked repos from a github user? [y/N] " response
-
-case "$response" in
-    [yY][eE][sS]|[yY]) 
-        clone_user_repos
-        ;;
-esac
-
 clone_user_repos() {
-    read -r -p 'Enter the github user name you want to clone repos from: ' githubUser
-    
-    for row in $(curl -s "https://api.github.com/users/${githubUser}/repos" | jq -r '.[] | select(.private==false) | select(.fork==false) | select(.archived==false) | .ssh_url'); do
-        clone() {
-            git clone $row
-        }
+    read 'githubUser?Enter the github user name you want to clone repos from: '
+    response=$(curl -s -f "https://api.github.com/users/${githubUser}/repos")
 
-        clone '.name'
-    done
+    if (($?)); then
+        echo "Request to github failed. Did you enter the correct github user?"
+    else
+        for row in $(echo $response | jq -r '.[] | select(.private==false) | select(.fork==false) | select(.archived==false) | .ssh_url'); do
+            git clone $row
+            echo ""
+        done
+
+        echo "✔ Cloning of all repos from ${githubUser} complete"
+    fi
 }
+
+# Core github setup loop
+ANSWER=0
+
+while ((!$ANSWER)); do
+    read -q "?Do you want to clone all public, non-archived, non-forked repos from a github user? [y/n] "
+    ANSWER=$?
+    echo ""
+
+    if ((!$ANSWER)); then
+        clone_user_repos
+    fi
+
+    echo ""
+done
+
+echo "\nEnd of github setup, returning to main menu..."
