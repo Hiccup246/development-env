7e32c2801e618ca63d8cb5e8757cd7f31f477f9d
--global <james.watt529@outlook.com>
2023-02-13 17:33:34 +0000

    Add loading spinner functions to main script, format main script selection menu, add github-setup script to main script

--- main.sh
+++ main.sh
@@ -2,21 +2,81 @@
 # Main program loop with user options
 
 echo "===Welcome to Hiccup246 development environment setup program==="
-echo "What do you want to do?"
 
+COLUMNS=12
 PS3="Select item please: "
 
-items=("Item 1" "Item 2" "Item 3")
-
+items=("Run entire mac setup" "Configure ssh keys" "Configure global github info" "Clone repos from github user")
+start_loading_spinner
 while true; do
     select item in "${items[@]}" Quit
     do
         case $REPLY in
-            1) echo "Selected item #$REPLY which means $item"; break;;
-            2) echo "Selected item #$REPLY which means $item"; break;;
-            3) echo "Selected item #$REPLY which means $item"; break;;
-            $((${#items[@]}+1))) echo "We're done!"; break 2;;
-            *) echo "Ooops - unknown choice $REPLY"; break;
+            1)
+                echo "Selected item #$REPLY which means $item";
+                break
+                ;;
+            2) 
+                echo "Selected item #$REPLY which means $item";
+                break
+                ;;
+            3) 
+                echo "Selected item #$REPLY which means $item";
+                break
+                ;;
+            4) 
+                chmod +x github-setup.sh && ./github-setup.sh
+                break
+                ;;
+            $((${#items[@]}+1)))
+                echo "We're done!";
+                break 2
+                ;;
+            *)
+                echo "Ooops - unknown choice $REPLY";
+                break
+                ;
         esac
     done
+
+    echo ""
 done
+
+# Loading spinner starter code
+# Ispiration from https://unix.stackexchange.com/questions/225179/display-spinner-while-waiting-for-some-process-to-finish
+
+LOADING_MESSAGE="Loading..."
+export LOADING_MESSAGE
+
+function cursorBack() {
+  echo -en "\033[$1D"
+}
+
+start_loading_spinner_braille() {
+    local spin='⣾⣽⣻⢿⡿⣟⣯⣷'
+    local charwidth=1
+    local i=0
+    tput civis # cursor invisible
+    
+    while true; do
+        local i=$(((i + $charwidth) % ${#spin}))
+        printf  "\r${spin:$i:$charwidth} ${LOADING_MESSAGE}"
+        cursorBack 1
+        sleep .1
+    done
+
+    tput cnorm
+}
+
+start_loading_spinner() {
+    spin='-\|/'
+    message="yes"
+    i=0
+
+    while true; do
+        i=$(( (i+1) %4 ))
+        printf "\r${spin:$i:1} ${LOADING_MESSAGE}"
+        sleep .1
+        message="no"
+    done
+}
