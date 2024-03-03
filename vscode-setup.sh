06c88a2d6317e8c29086dc2019ea9313da307b25
James Watt <james.watt529@outlook.com>
2024-03-03 15:29:36 +0000

    Make vscode-setup script use vscode-extensions.txt

--- vscode-setup.sh
+++ vscode-setup.sh
@@ -1,21 +1,4 @@
 #!/bin/zsh
-# Install program independent plugins
-
-code --install-extension mikestead.dotenv
-code --install-extension EditorConfig.EditorConfig
-code --install-extension tamasfe.even-better-toml
-code --install-extension denoland.vscode-deno
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension william-voyek.vscode-nginx
-code --install-extension dsznajder.es7-react-js-snippets
-code --install-extension Orta.vscode-jest
-code --install-extension ms-playwright.playwright
-code --install-extension rust-lang.rust-analyzer
-code --install-extension svelte.svelte-vscode
-code --install-extension Vue.vscode-typescript-vue-plugin
-code --install-extension Vue.volar
-code --install-extension ms-vscode.vscode-typescript-next
-code --install-extension stivo.tailwind-fold
+xargs code --install-extension < vscode-extensions.txt
 
 echo "✔ Generic Vscode plugins installed"
