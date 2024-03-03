80eee9905565a4e95ffa135bf6bb587ac817f989
James Watt <james.watt529@outlook.com>
2024-03-03 15:53:24 +0000

    Make homebrew-setup use configuration files, remove redundant comments

--- homebrew-setup.sh
+++ homebrew-setup.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-# Install homebrew and all packages
 
 # Install homebrew package manager
 if hash brew; then
@@ -10,36 +9,8 @@ else
 	echo "✔ Homebrew installed and updated"
 fi
 
-# Install command line programs
-# Shells
-brew install zsh
-brew install bash
-
-# Command line utilities
-brew install curl
-brew install wget
-brew install coreutils
-brew install jq
-
-# Code repostiry
-brew install git
-
-# Package managers
-brew install pnpm
-brew install npm
-
-# GUI Application installer
-brew install mas
-
-# Version managers
-brew install asdf
-brew install rustup-init
-
-# Optional installations
-brew install railway
-brew install docker
-brew install nginx
-brew install flyctl
+# Install all home brew packages
+xargs brew install < homebrew.txt
 
 # Install GUI applications
 # --appdir="/Applications" installs applications for all users
@@ -47,23 +18,7 @@ brew install flyctl
 
 read -q "?Do you want to install desktop GUI applications using homebrew? [y/n]"
 if ((!$?)); then
-	brew install --cask --appdir="/Applications" spotify
-	brew install --cask --appdir="/Applications" discord
-	brew install --cask --appdir="/Applications" visual-studio-code
-	brew install --cask --appdir="/Applications" google-chrome
-	brew install --cask --appdir="/Applications" postman
-	brew install --cask --appdir="/Applications" firefox
-	brew install --cask --appdir="/Applications" docker
-	brew install --cask --appdir="/Applications" microsoft-teams
-	brew install --cask --appdir="/Applications" slack
-	brew install --cask --appdir="/Applications" bitwarden
-	brew intsall --cask --appdir="/Applications" raycast
-
-	if [[ ! -d /Applications/iTerm.app ]]; then
-		brew install --cask --appdir="/Applications" iterm2
-	else
-		echo "You already have iTerm2 installed."
-	fi
+	grep -e '^[^#]' homebrew-cask.txt | xargs brew install --cask --appdir="/Applications"
 fi
 
 echo ""
